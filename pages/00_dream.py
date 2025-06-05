e0a3f3b765a5b8bbf717bc83d1734eb474189fb5
minseoLove <lovingbella0605@gmail.com>
2025-06-05 16:00:37 +0900

    Update 00_dream.py

--- pages/00_dream.py
+++ pages/00_dream.py
@@ -1,5 +1,3 @@
-import streamlit as st
-st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2QzeHA1YnppcDduNDY4bTRrZzM0dmpmZmxvdGcxc3h6dGlxbzVzbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/tFSqMSMnzPRTAdvKyr/giphy.gif")
 
 import streamlit as st
 import yfinance as yf
