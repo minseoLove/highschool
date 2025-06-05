f5b1c1b496746d7be7b0f5e038d49906c1b25b72
minseoLove <lovingbella0605@gmail.com>
2025-06-05 15:46:14 +0900

    Create 00_dream.py

---
+++ pages/00_dream.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.image("https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZ2QzeHA1YnppcDduNDY4bTRrZzM0dmpmZmxvdGcxc3h6dGlxbzVzbiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/tFSqMSMnzPRTAdvKyr/giphy.gif")
